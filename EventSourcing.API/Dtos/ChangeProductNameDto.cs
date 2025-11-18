c5acd8bc6218d9ab2dc9b27e831e94584be7227c
Seyhan Çağlaroğlu <seyhancaglaroglu@gmail.com>
2025-11-18 09:28:36 +0300

    Event Sourcing Completed 2

--- EventSourcing.API/Dtos/ChangeProductNameDto.cs
+++ EventSourcing.API/Dtos/ChangeProductNameDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace EventSourcing.API.Dtos
 {
     public class ChangeProductNameDto
-    {
+    {//
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
     }
